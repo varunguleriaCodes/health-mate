4238280a074f0e38394726bb0d29470a625e95ec
Varun Guleria <varunguleria01@gmail.com>
2024-12-14 01:47:38 +0530

    feat: dark_mode+ui_updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
+    <div>
       <ToastContainer />
       <Navbar />
       <Routes>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
+import DarkMode from '../components/darkMode'
 
 const Home = () => {
   return (
